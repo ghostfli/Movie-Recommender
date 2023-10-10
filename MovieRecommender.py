3f08d6ee9e276f3656c5de65ed2a0482593e2501
Geoffrey Brendel <geoff@pektel.com>
2023-10-09 23:13:00 -0700

    Implemented create_genre_vector which creates the average ratings for each genre rated by the user within the Movie Recommender class. Also implemented a print function for that vector.

--- MovieRecommender.py
+++ MovieRecommender.py
@@ -6,6 +6,8 @@ class MovieRecommender():
     def __init__(self, userid: int) -> None:
         self.userid = userid
         self.ratings = self.get_ratings()
+        self.genre_vector = []
+        
 
     def get_ratings(self):
         ratings = []
@@ -15,12 +17,51 @@ class MovieRecommender():
                 ratings.append((row['movieId'], row['rating']))
         return ratings
     
-    def print_movies_review_and_rating(self):
+    def print_movie_genre_and_rating(self):
         movies = Movies()
         for movie in self.ratings:
             row = movies.df.loc[movies.df['movieId'] == movie[0]]
-            print(row['movieId'].to_string(index=False), row['title'].to_string(index=False), movie[1])
+            print(row['movieId'].to_string(index=False), row['title'].to_string(index=False), row['genres'].to_string(index=False), movie[1])
+
+    def print_genre_vector(self):
+        for tuple in self.genre_vector:
+            print(tuple)
+
+
+    def create_genre_vector(self):
+        genre_vec = []
+        movies = Movies()
+        for movie in self.ratings:
+            row = movies.df.loc[movies.df['movieId'] == movie[0]]
+            #Split genre by | delimeters
+            genres = row['genres'].to_string(index=False).split('|')
+            for genre in genres:
+                tuple_exists = False
+                for genre_list in genre_vec:
+                    if (genre_list[0] == genre):
+                        #Indicates we have found a existing tuple for this genre
+                        tuple_exists = True
+                        #genre_tuple[1] : contains sum of ratings for this genre
+                        genre_list[1] += movie[1]
+                        #genre_tuple[2] : number of ratings for this genre
+                        genre_list[2] += 1
+                        #genre_tuple[3] : genre_tuple[1] / genre_tuple[2]
+                        genre_list[3] = genre_list[1] / genre_list[2]
+                        break
+
+                if (tuple_exists == False):
+                    #Create a tuple then
+                    new_list = [genre, movie[1], 1, movie[1]]
+                    genre_vec.append(new_list)
+                
+            
+        self.genre_vector = genre_vec
+
+
+
 
 if __name__ == "__main__":
     MR = MovieRecommender(1)
-    MR.print_movies_review_and_rating()
+    #MR.print_movie_genre_and_rating()
+    MR.create_genre_vector()
+    MR.print_genre_vector()
